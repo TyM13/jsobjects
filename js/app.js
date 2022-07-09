fdee69a8ec78a2cc04b18cbec3bd229de130d323
Your Name <you@example.com>
2022-07-09 14:52:49 -0600

    got nowhere need to do more work

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
+let counter = 0;
 let company = {
     name: `Tyler`,
     yearly_revenue: 1000000,
     ceo: `Important guy`,
-    number_of_employees: 58
-    
-};if (yearly_revenue >=1000000)
-console.log $(company[`name`] $(company[`ceo`]));
+    number_of_employees: 58,
+    managers: [`ty`, `alex`, `random dude`]
+
+};while(counter < managers.length) {
+    console.log(managers[counter]);
+    counter = counter +1;
+
+}if (company[`yearly_revenue`] >= 1000000) {
+    console.log(`congrats to: ${company[`name`]}, ceo: ${company[`ceo`]} employees: ${company[`number_of_employees`]}`);
+} else {
+    console.log(`you can do better`);
+}
