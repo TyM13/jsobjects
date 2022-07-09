ad30a3155a8d25fefd4cc6466f6b7af9488ce7ec
Your Name <you@example.com>
2022-07-09 13:46:08 -0600

    jsobjects

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let company = {
+    name: `Tyler`,
+    yearly_revenue: 1000000,
+    ceo: `Important guy`,
+    number_of_employees: 58
+    
+};if (yearly_revenue >=1000000)
+console.log $(company[`name`] $(company[`ceo`]));
